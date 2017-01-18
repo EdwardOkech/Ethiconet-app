1dbbea12464a31190bb15618bb8f0ca09a73ad81
EdwardOkech <okechjobs@gmail.com>
2017-01-19 00:09:51 +0300

    some minor additions to accounts app

---
+++ apps/accounts/urls.py
@@ -0,0 +1,8 @@
+# from django.conf.urls import url 
+# #from django.contrib.flatpages import views
+# # from apps.accounts import views
+
+# urlpatterns = [
+#       url(r'^$', views.accounts, name='accounts'),
+      
+# ]
